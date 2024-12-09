475f54bae799ee2d41d75c0227ba616a5f18fee5
Katya1539 <erkenovakatya@mail.ru>
2024-12-09 23:17:51 +0300

    task 1

---
+++ src/test/java/ru/netology/page/TransferPage.java
@@ -0,0 +1,38 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class TransferPage {
+    public final SelenideElement transferButton = $("[data-test-id='action-transfer']");
+    public final SelenideElement amountInput = $("[data-test-id='amount'] input");
+    public final SelenideElement fromInput = $("[data-test-id='from'] input");
+    public final SelenideElement transferHead = $(byText("Пополнение карты"));
+    public final SelenideElement errorMessage = $("[data-test-id='error-notification'] .notification__content");
+
+    public TransferPage() {
+        transferHead.shouldBe(visible);
+    }
+
+    public DashboardPage makeValidTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        makeTransfer(amountToTransfer, cardInfo);
+        return new DashboardPage();
+    }
+
+    public void makeTransfer(String amountToTransfer, DataHelper.CardInfo cardInfo) {
+        amountInput.setValue(amountToTransfer);
+        fromInput.setValue(cardInfo.getCardNumber());
+        transferButton.click();
+    }
+
+    public void findErrorMessage(String expectedText) {
+        errorMessage.shouldHave(text(expectedText), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+}
